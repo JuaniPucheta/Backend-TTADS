7a619d7c95e9abc6ec6bdf459d916406ccebc6d1
Luchoragusa <luchito.rc4@gmail.com>
2022-06-21 16:10:03 -0300

    put / update

--- Rutas/api/usuarios.js
+++ Rutas/api/usuarios.js
@@ -13,4 +13,10 @@ router.post('/', async (req, res) =>{
     res.json(usuarios); // aca me la devuelve para ver q onda
 } );
 
+router.put('/:id', async (req, res) =>{
+    await Usuario.update(req.body, {
+        where: {id: req.params.id}}) // actualizo el usuario que tenga esa id con los datos que trae la api
+    res.json({success: 'Usuario actualizado'});
+} );
+
 module.exports = router;
