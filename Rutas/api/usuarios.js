cb96649ac112f0046b16c6708c82fd6a35e5480f
Luchoragusa <luchito.rc4@gmail.com>
2022-06-21 16:02:10 -0300

    post

--- Rutas/api/usuarios.js
+++ Rutas/api/usuarios.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const { Usuario } = require('../../db') // traigo la entidad usuarios
 
+// localhost:3000/api/usuarios
+
 router.get('/', async (req, res) =>{
     const usuarios = await Usuario.findAll(); // devuelve una promesa
     res.json(usuarios);
 } );
 
-router.get('/', async (req, res) =>{
-    const usuarios = await Usuario.create(req.body)
-    res.json(usuarios);
+router.post('/', async (req, res) =>{
+    const usuarios = await Usuario.create(req.body) // creo al usuario con los datos que trae la api
+    res.json(usuarios); // aca me la devuelve para ver q onda
 } );
 
 module.exports = router;
